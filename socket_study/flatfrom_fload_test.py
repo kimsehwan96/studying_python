e260fd610818fafa9cb3226158ccfe34d6676617
kimsehwanestat <sktpghks138@gmail.com>
2020-08-05 21:12:07 +0900

    add test logic for sending float data through udp

---
+++ socket_study/flatfrom_fload_test.py
@@ -0,0 +1,19 @@
+import socket
+import random
+import struct
+from time import sleep
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(('127.0.0.1',5006)) #for nazare flatform
+
+while True:
+    float_data = random.uniform(1,100) #for random float
+    data = struct.pack('f', float_data)
+    try:
+        sock.sendto(data,('127.0.0.1',5006))
+    except Exception as e:
+        print("data was sent {}".format(data))
+    sleep(1) # delay for 1 second
+
+# for test one float data test logic.
