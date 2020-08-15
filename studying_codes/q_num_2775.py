2bb6a44239018b5a1f00a567c4792445128d7b6d
kimsehwanestat <sktpghks138@gmail.com>
2020-08-16 03:32:54 +0900

    sokved 2775

--- studying_codes/q_num_2775.py
+++ studying_codes/q_num_2775.py
@@ -1,33 +1,29 @@
-# 0층의 i호에는 i명이 산다. 
-# 01호 1명, 02호 2명 
-'''
-1명 4명 10명 20 35 56 84  110          2층   floor[2]
-1명 3명 6명 10명 15 21 28 36     1층  floor[1]
-1명 2명 3명 4명 5명 6명 7명 8명 ... 0층  floor[0]
-'''
-def zero_floor_table(n):
-    table = []
-    for i in range(n):
-        table.append(i+1)
+zero_floor = [ x+1 for x in range(15) ]
+
+def make_table():
+    table = {}
+    table[1] = zero_floor
+    for floor in range(15):
+        if floor == 0:
+            table[floor] = zero_floor
+        else:
+            lower_floor = table.get(floor-1)
+            current_floor = []
+            stack = 0
+            for value in lower_floor:
+                stack += value
+                current_floor.append(stack)
+            table[floor] = current_floor
     return table
 
-def calculate(k, n):
-    # k는 층수, n은 호수
-    floor = []
-    tmp_list = []
-    floor.append(zero_floor_table(n))
-    for idx in range(k):
-        floor.append([])
-        for i in range(n):
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
+    table = make_table() #dict
+    #print(table)
     testNum = int(input())
-    calculate(1,3)
+    for idx in range(testNum):
+        result = None 
+        k = int(input())
+        n = int(input())
+        result = table.get(k)[n-1]
+        print(result)
+        
