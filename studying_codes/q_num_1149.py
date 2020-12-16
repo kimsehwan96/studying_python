ffb6cd6a78f2b59a73ad516b0d705a3a5d9c9c12
Kimsehwan96 <sktpghks138@gmail.com>
2020-12-17 00:42:46 +0900

    sovled 7568

--- studying_codes/q_num_1149.py
+++ studying_codes/q_num_1149.py
@@ -1,3 +1,17 @@
 house = int(input())
-costR, costG, costB = list(map(int, input().split(' ')))
+colors = []
+for _ in house:
+    costR, costG, costB = list(map(int, input().split(' ')))
+    colors.append(costR, costG, costB)
+
+dp = [x for x in range(house)]
+
+# 빨 파 초 빨 파 초 빨 파 초
+# 시작하는 색이 뭐냐가 중요한거같고.. 
+# 뒤에 어떤 가격이 나올지 모르기 때문에 
+# 전에 계산한 값들을 메모해놔야겠네.
+# 아닌가.
+
+for v in colors:
+
 
--- studying_codes/q_num_7568.py
+++ studying_codes/q_num_7568.py
@@ -1,13 +1,45 @@
-test_case = 5
+"""
+입력
+5
+55 185
+58 183
+88 186
+60 175
+46 155
 
-info =[]
-for x in range(test_case):
-    tmp = map(int, input().split())
-    info.append(list(tmp))
-    #중첩 리스트 생성
+출력
+2 2 1 2 5
+"""
+import sys
+
+def find_rank(infos:list):
+    ranking = []
+    for v in infos:
+        rank = 1
+        w = v[0]
+        h = v[1]
+        for v_c in infos:
+            if v_c == v:
+                continue
+            else:
+                w_comp = v_c[0]
+                h_comp = v_c[1]
+                if ((w < w_comp) & (h < h_comp)):
+                    rank += 1
+                else:
+                    pass
+        ranking.append(rank)
+    return ranking
 
 
-for n in range(len(info)):
-    order = 1
-    if info[n][0] > 
-
+if __name__ == "__main__":
+    infos = []
+    dc = [] #dc[0] = weight, dc=[1] = height
+    test_case =  int(input())
+    for _ in range(test_case):
+        dc = list(map(int, input().split(" ")))
+        infos.append(dc)
+    
+    ranking = find_rank(infos)
+    for _ in ranking:
+        print( _ , end = ' ')
